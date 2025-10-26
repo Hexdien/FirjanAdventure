6417997dace7695206811edfce056dfe9e8e47d6
Hexdien <hexdien@proton.me>
2025-10-25 22:39:28 -0300

    arquivos do mapa

--- src/main/resources/xml/grass.tsx
+++ src/main/resources/xml/grass.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="grass" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../static/assets/tilesets/grass.png" width="32" height="32"/>
+<tileset version="1.10" tiledversion="1.11.2" name="turfs" tilewidth="32" tileheight="32" tilecount="110" columns="11">
+ <image source="../static/assets/turfs.png" width="352" height="320"/>
 </tileset>
