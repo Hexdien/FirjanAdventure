49fce133b9fc1e9333991b43fe93f6585cbbd35d
Hexdien <hexdien@proton.me>
2025-10-30 16:26:12 -0300

    map fixes

--- src/main/resources/static/assets/maps/turf.tsx
+++ src/main/resources/static/assets/maps/turf.tsx
@@ -1,4 +1,4 @@
-<? xml version = "1.0" encoding = "UTF-8" ?>
-  <tileset version="1.10" tiledversion="1.11.2" name="Turf" tilewidth="32" tileheight="32" tilecount="110" columns="11">
-    <image source="../tilesets/turfs.png" width="352" height="320" />
-  </tileset>
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="turfs" tilewidth="32" tileheight="32" tilecount="110" columns="11">
+ <image source="../tilesets/turfs.png" width="352" height="320"/>
+</tileset>
