cba3ea97c8eb62a7f38eda882f02cc2729a318a7
Hexdien <hexdien@proton.me>
2025-11-07 04:26:12 -0300

    removendo garbage

--- src/main/java/com/firjanadventure/firjanadventure/modelo/Minotauro.java
+++
@@ -1,9 +0,0 @@
-package com.firjanadventure.firjanadventure.modelo;
-
-public class Minotauro extends Monstro {
-
-    public Minotauro(int level) {
-        super("Minotauro", 65, level, 20, 40);
-
-    }
-}
