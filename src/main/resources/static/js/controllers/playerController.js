46404aec225a7003e6494a931d4ef95008f5ccf2
Hexdien <hexdien@proton.me>
2025-11-10 04:26:41 -0300

    feat: encapsulando controles do player

---
+++ src/main/resources/static/js/controllers/playerController.js
@@ -0,0 +1,81 @@
+
+
+export function setupPlayerController(k, player) {
+
+  // Atualização da camera 
+  let tick = 0;
+  k.onUpdate(() => {
+    k.camPos(player.worldPos());
+    k.camScale(2); // ou 1
+    tick++;
+  });
+
+
+  // Movimentação 
+  function setSprite(player, spriteName) {
+    if (player.currentSprite !== spriteName) {
+      player.use(k.sprite(spriteName));
+      player.currentSprite = spriteName;
+    }
+  }
+
+  k.onKeyDown("down", () => {
+    if (player.isInDialogue) return;
+    if (player.curAnim() !== "walk") {
+      setSprite(player, "player-down");
+      player.play("walk");
+    }
+    player.move(0, player.speed);
+  });
+
+  k.onKeyDown("up", () => {
+
+    if (player.isInDialogue) return;
+    if (player.curAnim() !== "walk") {
+      setSprite(player, "player-up");
+      player.play("walk");
+    }
+    player.move(0, -player.speed);
+  });
+
+
+  k.onKeyDown("left", () => {
+    if (player.isInDialogue) return;
+    if (player.curAnim() !== "walk") {
+      setSprite(player, "player-left");
+      player.play("walk");
+    }
+    player.move(-player.speed, 0);
+  });
+
+  k.onKeyDown("right", () => {
+    if (player.isInDialogue) return;
+    if (player.curAnim() !== "walk") {
+      setSprite(player, "player-right");
+      player.play("walk");
+    }
+    player.move(player.speed, 0);
+  });
+
+  k.onKeyRelease("left", () => {
+    setSprite(player, "player-idle-left");
+    player.play("idle");
+  });
+
+  k.onKeyRelease("right", () => {
+    setSprite(player, "player-idle-right");
+    player.play("idle");
+  });
+  k.onKeyRelease("up", () => {
+    setSprite(player, "player-idle-back");
+    player.play("idle");
+  });
+
+  k.onKeyRelease("down", () => {
+    setSprite(player, "player-idle-front");
+    player.play("idle");
+  });
+
+
+
+}
