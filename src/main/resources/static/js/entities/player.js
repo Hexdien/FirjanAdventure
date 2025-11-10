505da55c884cdc6f0f12e966fef69f1c70c0423a
Hexdien <hexdien@proton.me>
2025-11-10 04:27:00 -0300

    feat: encapsulando logica para instanciar player

---
+++ src/main/resources/static/js/entities/player.js
@@ -0,0 +1,20 @@
+
+
+export function createPlayer(k, position) {
+  let player = k.add([
+    k.sprite("player-idle-front", { anim: "idle" }),
+    k.pos(position),
+    k.scale(1),
+    k.body(),
+    k.area(),
+    k.anchor("bot"),
+    {
+      speed: 300,       // Andando
+      isRunning: false,
+      speedRunning: 600,// Correndo
+      isInDialogue: false,
+    },
+  ])
+
+  return player;
+}
