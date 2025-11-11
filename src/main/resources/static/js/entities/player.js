e6acf0fbb5d4689ce2306d0b61e34b7a1b7eca8a
Hexdien <hexdien@proton.me>
2025-11-11 05:18:10 -0300

    feat: Spawn configurado

--- src/main/resources/static/js/entities/player.js
+++ src/main/resources/static/js/entities/player.js
@@ -1,9 +1,17 @@
+function spawn(ctx, position) {
+  const posX = ctx.pos.x;
+  const posY = ctx.pos.y;
+  if (posX === 0 && posY === 0) {
+    return position;
+  }
+  return [posX, posY];
 
+}
 
-export function createPlayer(k, position) {
+export function createPlayer(k, ctx, position) {
   let player = k.add([
     k.sprite("player-idle-front", { anim: "idle" }),
-    k.pos(position),
+    k.pos(spawn(ctx, position)),
     k.scale(1),
     k.body(),
     k.area(),
