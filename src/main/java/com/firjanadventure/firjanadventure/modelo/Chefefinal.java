dd5e31662f2c6bebf4d94dd24c78332340d18609
Hexdien <hexdien@proton.me>
2025-11-15 04:48:53 -0300

    delete: codigo velho removido
    chefe final será introduzido de outro modo

--- src/main/java/com/firjanadventure/firjanadventure/modelo/Chefefinal.java
+++
@@ -1,7 +0,0 @@
-package com.firjanadventure.firjanadventure.modelo;
-public class Chefefinal extends Monstro {
-
-    public Chefefinal() {
-        super("Cthulhu", 200, 5, 100, 200);
-    }
-}
--- src/main/java/com/firjanadventure/firjanadventure/modelo/Item.java
+++ src/main/java/com/firjanadventure/firjanadventure/modelo/Item.java
@@ -1,7 +1,5 @@
 package com.firjanadventure.firjanadventure.modelo;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
