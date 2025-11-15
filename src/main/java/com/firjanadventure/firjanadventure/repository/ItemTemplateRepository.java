cd951846e73e699c398077afe46e457d2a3073d8
Hexdien <hexdien@proton.me>
2025-11-15 04:48:06 -0300

    feat: repositorios de template item e monstro

---
+++ src/main/java/com/firjanadventure/firjanadventure/repository/ItemTemplateRepository.java
@@ -0,0 +1,9 @@
+package com.firjanadventure.firjanadventure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.firjanadventure.firjanadventure.modelo.ItemTemplate;
+
+public interface ItemTemplateRepository extends JpaRepository<ItemTemplate, Long> {
+
+}
---
+++ src/main/java/com/firjanadventure/firjanadventure/repository/MonsterTemplateRepository.java
@@ -0,0 +1,8 @@
+package com.firjanadventure.firjanadventure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.firjanadventure.firjanadventure.modelo.MonsterTemplate;
+
+public interface MonsterTemplateRepository extends JpaRepository<MonsterTemplate, Long> {
+}
