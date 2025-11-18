2d484d79c7596da170ab37b9f74a31ba3980a961
Hexdien <hexdien@proton.me>
2025-11-18 20:19:28 -0300

    refact: modificando estado e turnoAtual para tipos Enum

--- src/main/java/com/firjanadventure/firjanadventure/web/dto/BattleStateResponse.java
+++ src/main/java/com/firjanadventure/firjanadventure/web/dto/BattleStateResponse.java
@@ -1,13 +1,16 @@
 package com.firjanadventure.firjanadventure.web.dto;
 
+import com.firjanadventure.firjanadventure.modelo.enums.EstadoBatalha;
+import com.firjanadventure.firjanadventure.modelo.enums.TurnoBatalha;
+
 public record BattleStateResponse(
     Long battleId,
     int monsterHp,
     int monsterAtk,
     int monsterDef,
 
-    String estado,
-    String turnoAtual
+    EstadoBatalha estado,
+    TurnoBatalha turnoAtual
 
 ) {
 }
