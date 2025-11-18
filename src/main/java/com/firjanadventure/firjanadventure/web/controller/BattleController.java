f6b6ca8173d55543893ba499043bee2d4e7b73b9
Hexdien <hexdien@proton.me>
2025-11-18 01:02:16 -0300

    feat: controller para receber requisição do ataque

--- src/main/java/com/firjanadventure/firjanadventure/web/controller/BattleController.java
+++ src/main/java/com/firjanadventure/firjanadventure/web/controller/BattleController.java
@@ -1,16 +1,16 @@
 package com.firjanadventure.firjanadventure.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.firjanadventure.firjanadventure.service.BattleService;
-import com.firjanadventure.firjanadventure.web.dto.BattleStateResponse;
-import com.firjanadventure.firjanadventure.web.dto.MonsterSpawnRequest;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firjanadventure.firjanadventure.service.BattleService;
+import com.firjanadventure.firjanadventure.web.dto.BattleAttackReq;
+import com.firjanadventure.firjanadventure.web.dto.BattleStateResponse;
+import com.firjanadventure.firjanadventure.web.dto.MonsterSpawnRequest;
 
 @RestController
 @RequestMapping("/api/batalha")
@@ -27,6 +27,13 @@ public class BattleController {
       @PathVariable Long personagemId) {
     var instance = service.iniciarBatalha(request, personagemId);
     return ResponseEntity.ok(instance);
+  }
+
+  @PostMapping("/{battleId}/atacar")
+  public ResponseEntity<BattleStateResponse> processarBatalha(@PathVariable Long battleId,
+      @RequestBody BattleAttackReq req) {
+    var body = service.processarBatalha(battleId, req);
+    return ResponseEntity.ok(body);
   }
 
 }
