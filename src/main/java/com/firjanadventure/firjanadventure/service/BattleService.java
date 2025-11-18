64283a7cfce4f7e7d54c1028fec653eea00209ab
Hexdien <hexdien@proton.me>
2025-11-18 01:03:19 -0300

    feat: lógica pesada de batalha e regras de negócio

--- src/main/java/com/firjanadventure/firjanadventure/service/BattleService.java
+++ src/main/java/com/firjanadventure/firjanadventure/service/BattleService.java
@@ -1,11 +1,14 @@
 package com.firjanadventure.firjanadventure.service;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.springframework.stereotype.Service;
 
 import com.firjanadventure.firjanadventure.modelo.Batalha;
 import com.firjanadventure.firjanadventure.modelo.Personagem;
 import com.firjanadventure.firjanadventure.repository.BatalhaRepository;
 import com.firjanadventure.firjanadventure.repository.PersonagemRepository;
+import com.firjanadventure.firjanadventure.web.dto.BattleAttackReq;
 import com.firjanadventure.firjanadventure.web.dto.BattleStateResponse;
 import com.firjanadventure.firjanadventure.web.dto.MonsterInstance;
 import com.firjanadventure.firjanadventure.web.dto.MonsterSpawnRequest;
@@ -42,11 +45,71 @@ public class BattleService {
         "INICIADA",
         "PLAYER");
 
-    String Estado = batalha.getEstado();
+    String estado = batalha.getEstado();
     String turnoAtual = batalha.getTurnoAtual();
     batalhaRepo.save(batalha);
 
-    return new BattleStateResponse(Estado, turnoAtual);
+    return new BattleStateResponse(
+        batalha.getId(),
+        batalha.getMonstroHpAtual(),
+        batalha.getMonstroAtk(),
+        batalha.getMonstroDef(),
+        estado,
+        turnoAtual
+
+    );
+
+  }
+
+  public BattleStateResponse processarBatalha(Long battleId, BattleAttackReq req) {
+    Batalha batalha = batalhaRepo.findById(battleId)
+        .orElseThrow(() -> new RuntimeException("ID da batalha não existe!"));
+    Personagem personagem = personRepo.findById(req.personagemId())
+        .orElseThrow(() -> new RuntimeException("ID do personagem não existe!"));
+
+    if (batalha.getEstado() == "FINALIZADA") {
+      new RuntimeException("Batalha ja foi finalizada!");
+    }
+
+    // batalha.setEstado("EM_ANDAMENTO");
+
+    if (batalha.getTurnoAtual() == "PLAYER") {
+      if (req.acao() == "ATACAR") {
+        int danoFinal = calcularDano(personagem.getAtributo("forca"), batalha.getMonstroDef(), req.tipoAtaque());
+        int hpFinal = batalha.getMonstroHpAtual() - danoFinal;
+        String estado = "EM_ANDAMENTO";
+        String turnoAtual = "MONSTER";
+        return new BattleStateResponse(battleId,
+            hpFinal,
+            batalha.getMonstroAtk(),
+            batalha.getMonstroDef(),
+            estado, turnoAtual) btlResp;
+      }
+    } else {
+      int danoFinal = calcularDano(batalha.getMonstroAtk(), personagem.getAtributo("defesa"), "FISICO");
+      personagem.setAtributo("hp", personagem.getAtributo("hp") - danoFinal);
+      String turnoAtual = "PLAYER";
+return new BattleStateResponse(battleId,
+            hpFinal,
+            batalha.getMonstroAtk(),
+            batalha.getMonstroDef(),
+            estado, turnoAtual) btlResp;
+
+
+    }
+return btlResp;
+  
+
+
+  }
+
+  public static int calcularDano(int ataque, int defesa, String tipoAtaque) {
+    int danoBase = ataque - defesa;
+    int calcRand = ThreadLocalRandom.current().nextInt(0, ataque);
+    if ("MAGIA".equals(tipoAtaque)) {
+      danoBase += 15;
+    }
+    return danoBase + calcRand;
 
   }
 
