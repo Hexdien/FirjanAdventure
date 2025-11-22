f310da9c682a934433399b3ee8900f9848415a15
Hexdien <hexdien@proton.me>
2025-11-22 20:24:18 -0300

    feat: adicionado pontos de status na barra lateral

--- src/main/resources/static/js/entities/statsUpdate.js
+++ src/main/resources/static/js/entities/statsUpdate.js
@@ -9,6 +9,7 @@ export function statsUpdate(ctx) {
   document.getElementById("player-maxhp").textContent = ctx.atributos.hpMax;
   document.getElementById("player-attack").textContent = ctx.atributos.forca;
   document.getElementById("player-armor").textContent = ctx.atributos.defesa;
+  document.getElementById("player-statPoints").textContent = ctx.atributos.statPoints;
   document.getElementById("player-xp-canvas").textContent = ctx.atributos.xp;
   document.getElementById("player-xp-required-canvas").textContent = 100;
   document.getElementById("xp-bar-fill-canvas").style.width = ((ctx.atributos.xp / 100) * 100) + "%";
