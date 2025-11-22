3de118a11ce4e9fcd6f6a766196a3bd1bbf521fe
Hexdien <hexdien@proton.me>
2025-11-22 04:59:04 -0300

    feat: novas atualizações no HTML

--- src/main/resources/static/js/entities/statsUpdate.js
+++ src/main/resources/static/js/entities/statsUpdate.js
@@ -9,4 +9,9 @@ export function statsUpdate(ctx) {
   document.getElementById("player-maxhp").textContent = ctx.atributos.hpMax;
   document.getElementById("player-attack").textContent = ctx.atributos.forca;
   document.getElementById("player-armor").textContent = ctx.atributos.defesa;
+  document.getElementById("player-xp-canvas").textContent = ctx.atributos.xp;
+  document.getElementById("player-xp-required-canvas").textContent = 100;
+  document.getElementById("xp-bar-fill-canvas").style.width = ((ctx.atributos.xp / 100) * 100) + "%";
+
+
 }
