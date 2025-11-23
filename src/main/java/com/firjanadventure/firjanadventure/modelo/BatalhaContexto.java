27429e51f3dee5dbffbb2d9d3c8f73f9a3ec58e9
Hexdien <hexdien@proton.me>
2025-11-23 18:22:16 -0300

    delete: codigo inutilizado

--- src/main/java/com/firjanadventure/firjanadventure/modelo/BatalhaContexto.java
+++
@@ -1,9 +0,0 @@
-package com.firjanadventure.firjanadventure.modelo;
-
-import com.firjanadventure.firjanadventure.web.dto.MonsterInstance;
-
-public class BatalhaContexto {
-  private Batalha b;
-  private Personagem p;
-  private MonsterInstance m;
-}
