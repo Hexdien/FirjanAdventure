83d5930e7569c6564274e63d56ee2627f4e98895
Hexdien <hexdien@proton.me>
2025-11-26 00:20:18 -0300

    feat: atualizando setup de cena para lidar com mapa introdução

--- src/main/resources/static/js/entities/setupScene.js
+++ src/main/resources/static/js/entities/setupScene.js
@@ -1,20 +1,27 @@
 
 import { setupPlayerController } from "../controllers/setupPlayerController.js";
+import { introScene } from "../scenes/introScene.js";
 import { createPlayer } from "./player.js";
 import { statsUpdate } from "./statsUpdate.js";
 
 export function setupScene(k, ctx, position, mapConfig) {
 
 
-  // Inicializando objeto player dentro do contexto
-  ctx.player = createPlayer(k, position, ctx, mapConfig);
 
-  // Configurando controles do player
-  setupPlayerController(k, ctx);
+  if (ctx.atributos.mapZ === "intro") {
+    console.log("INTRO"); // Cena intro
+    introScene(k, ctx);
 
-  // Atualizando vizualização HTML 
-  statsUpdate(ctx);
+  } else {
+    // Inicializando objeto player dentro do contexto
+    ctx.player = createPlayer(k, position, ctx, mapConfig);
 
+    // Configurando controles do player
+    setupPlayerController(k, ctx);
+
+    // Atualizando vizualização HTML 
+    statsUpdate(ctx);
+  }
 
 
 
