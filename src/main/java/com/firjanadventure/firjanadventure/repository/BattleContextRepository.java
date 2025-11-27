91341ab9939f6711dae190bebbdfe124cea138a1
Hexdien <hexdien@proton.me>
2025-11-27 04:34:22 -0300

    feat: voltando para a arquitetura de contexto da batalha salvo em H2

--- src/main/java/com/firjanadventure/firjanadventure/repository/BattleContextRepository.java
+++ src/main/java/com/firjanadventure/firjanadventure/repository/BattleContextRepository.java
@@ -1,29 +1,8 @@
 package com.firjanadventure.firjanadventure.repository;
 
-import java.util.concurrent.ConcurrentHashMap;
-
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.firjanadventure.firjanadventure.modelo.BattleContext;
 
-@Component // Singleton controlado pelo Spring
-public class BattleContextRepository {
-
-  private final ConcurrentHashMap<Long, BattleContext> store = new ConcurrentHashMap<>();
-
-  public void save(Long battleId, BattleContext context) {
-    store.put(battleId, context);
-  }
-
-  public BattleContext get(Long battleId) {
-    return store.get(battleId);
-  }
-
-  public void remove(Long battleId) {
-    store.remove(battleId);
-  }
-
-  public BattleContext find(Long id) {
-    return store.get(id);
-  }
+public interface BattleContextRepository extends JpaRepository<BattleContext, Long> {
 }
